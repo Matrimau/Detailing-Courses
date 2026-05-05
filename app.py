f0c8c40ba4905c8b24f6d9118ecf2994e795afcc
fabel <fabel135@gmail.com>
2026-05-05 17:38:04 +0300

    route registration

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import request
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html', name='Fabel')
+    return render_template('Thanatos.html', name='Fabel')
 @app.route('/courses')
 def courses():
     return 'это твои курсы'
@@ -16,10 +16,18 @@ def hello():
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
-    psw = request.form['psw']
+    password = request.form['password']
     if username == 'admin' and psw == '1234':
         return f'вы вошли'
     else:
         return f'не правильный логин или пароль, повторите попытку'
-if __name__=='__main__':
+@app.route('/registration', methods=['POST'])
+def registration():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if not username or not password:
+        return {"status": "Неправильный логин или пароль"}
+    else:
+        return {"status": "Заходь"}
+if __name__ == '__main__':
     app.run(debug=True)
