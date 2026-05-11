ba9a841e2009251d89ebda358dabc096daa55fb7
Matrimau <guide.man@bk.ru>
2026-05-12 00:29:45 +0300

    fix: removed swearings
    fix: removed unnecessary var pass 2
    feat: added endpoint "about"

--- Izanagi.js
+++ Izanagi.js
@@ -1,11 +1,15 @@
 function showpage(name){
     document.getElementById('regi').style.display = 'none';
     document.getElementById('logi').style.display = 'none';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
     document.getElementById(name).style.display = 'block';
+
+    window.location.hash = name;
 }
 
 function seterror(id, error){
-    element = document.getElementById(id);
+    let element = document.getElementById(id);
     element.getElementsByClassName('formerror')[0].innerHTML = error;
 }
 
@@ -18,24 +22,27 @@ function validateForm(){
     }
     var pass = document.forms["reg"]["psw"].value;
     if (pass.length < 4){
-        seterror("pass", "Короткий пароль, аккаунт спиздят быстро");
+        seterror("pass", "Короткий пароль, нужно от 4 символов");
         returnval = false;
     }
 
     var pass2 = document.forms["reg"]["psw2"].value;
     if (pass2.length < 4){
-        seterror("pass2", "Короткий пароль, аккаунт спиздят быстро");
+        seterror("pass2", "Короткий пароль, нужно от 4 символов");
+        returnval = false;
+    }
+    if (pass2 != pass){
+        seterror("pass2", "Пароли не совпадают, так нельзя чувак");
         returnval = false;
     }
 
-    var pass2 = document.forms["reg"]["psw2"].value;
-    if (pass2 != pass){
-        seterror("pass2", "Пароли не совпадают, нельзя так чувак");
-        returnval = false;
-    }
+    /*if(returnval){
+        document.getElementById('Nickna').innerHTML = nick;
+        showpage('home')
+    }*/
 
     if(returnval){
-        fetch('http/localhost:5000/registration', {
+        fetch('http://26.96.157.124:5000/registration', {
             method: 'POST',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
@@ -44,27 +51,44 @@ function validateForm(){
             })
         })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            if(data.status === "ok"){
+                document.getElementById('Nickna').innerHTML = nick;
+                localStorage.setItem('token', data.token)
+                showpage('home')
+            } else {
+                seterror("nick", data.error)
+            }
+        })
+        .catch(error => {
+            alert('Сервер временно недоступен, сорри бро');
+            console.error(error)
+        })
     }
 
-    return returnval;
+    return false;
 }
 
 function validateLogin(){
     var returnval = true;
     var nick = document.forms["log"]["username"].value;
     if(nick.length < 4){
-        seterror("log-nick", "Ты же блять делал аккаунт, 4 или больше буковок надо");
+        seterror("log-nick", "Никнейм должен состоять из 4 или более символов");
         returnval = false;
     }
     var pass = document.forms["log"]["psw"].value;
     if(pass.length < 4){
-        seterror("log-pass", "Имбецил, пароль короткий у тебя, 4 или больше символов надо");
+        seterror("log-pass", "У тебя пароль короткий, от 4 символов");
         returnval = false;
     }
 
+    /*if(returnval){
+        document.getElementById('Nickna').innerHTML = nick;
+        showpage('home')
+    }*/
+    
     if(returnval){
-        fetch('http/localhost:5000/login', {
+        fetch('http://26.96.157.124:5000/login', {
             method: 'POST',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
@@ -73,8 +97,21 @@ function validateLogin(){
             })
         })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            if(data.status === 'ok'){
+                document.getElementById('Nickna').innerHTML = nick;
+                localStorage.setItem('token', data.token)
+                showpage('home')
+            }
+            else {
+                seterror(data.id, data.error)
+            }
+        })
+        .catch(error => {
+            alert('Сервер временно недоступен, сорри бро');
+            console.error(error)
+        })
     }
 
-return returnval;
+return false;
 }
